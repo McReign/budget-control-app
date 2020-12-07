95b72f6fc316fe4d8515aee142c1d3107ec7c25e
Маньшин Дмитрий Александрович <Manishin.D.A@sberbank.ru>
2020-12-07 03:13:13 +0300

    Change port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 const app = require('./app');
 const config = require('./config');
 
-app.listen(config.port, config.host, () => {
-    console.log(`App is running on ${config.host}:${config.port}`);
-});
+app.listen(process.env.PORT);
